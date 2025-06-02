a45c536819065b4caf846ba9d14f8659d4e5d640
root <root@Melpc.localdomain>
2025-06-02 17:54:26 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/FluidSim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FluidSim.dir/code/demo.c.o"
+  "CMakeFiles/FluidSim.dir/code/demo.c.o.d"
+  "CMakeFiles/FluidSim.dir/code/solver.c.o"
+  "CMakeFiles/FluidSim.dir/code/solver.c.o.d"
+  "FluidSim"
+  "FluidSim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FluidSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/FluidSim.dir/code/solver.c.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/FluidSim.dir/code/solver.c.o: \
+  /mnt/d/Repos/FluidSim/code/solver.c
